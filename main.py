48b34577f59008c917a00866f77bc13f211fefd4
root <root@sh4d0w.sh4d0w>
2021-11-18 17:15:37 +0100

    Main file for testing

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+def executeCommand(command):
+    command = command.lower()
+    #Time functions
+    if("what time is it" in command):
+        print("Its some time now")
+        pass
+    elif("set alarm" in command):
+        print("Alarm set")
+    #Weather functions
+    elif("weather" in command):
+        print("There is some shitty weather outside")
+    #LED functions
+    elif("led" in command):
+        print("LED's")
+
+
+
+def listenCommand():
+    command = input("Tell me what i need to do:\n")
+    executeCommand(command)
+
+
+def awaitCommand():
+    awake = input("What do you want?\n")
+    awake = awake.lower()
+    if("manfred" in awake):
+        listenCommand()
+
+while(True):
+    awaitCommand()
