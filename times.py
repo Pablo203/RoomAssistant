16435029babf8cc8fe768ae36a619b412ee17c1b
root <root@sh4d0w.sh4d0w>
2021-11-27 21:07:47 +0100

    Commenting code

--- times.py
+++ times.py
@@ -1,4 +1,5 @@
 import datetime
+from playsound import playsound
 import time
 import os
 import sys
@@ -6,13 +7,16 @@ from time import sleep
 
 class Alarm:
     def __init__(self):
+        #User gives after how many hours and minutes he wish alarm to go off
         self.hours = input("In how many hours you want me to wake you up? ")
         self.minutes = input("In how many minutes you want me to wake you up? ")
     
     def setAlarm(self):
+        #Turns hours and minutes to seconds
         self.timeToWake = float(self.hours) * 3600 + float(self.minutes) * 60
 
     def evokeAlarm(self):
+        #Clones proccess then sleeps for earlier getted seconds and runs alarm
         pid = os.fork()
         if pid == 0:
             print("Running task")
@@ -26,11 +30,14 @@ class Alarm:
             print("Running another task")
 
     def runAlarm(self):
+        #When used plays alarm audio
+        playsound("Audio/alarm_2015.mp3")
         print("ALARM GOES OFFFFFFFF \n")
 
 def tellTime():
-        currentTime = datetime.datetime.now()
-        hours = currentTime.hour
-        minutes = currentTime.minute
-        print(hours)
-        print(minutes)
+    #Tells current time
+    currentTime = datetime.datetime.now()
+    hours = currentTime.hour
+    minutes = currentTime.minute
+    print(hours)
+    print(minutes)
