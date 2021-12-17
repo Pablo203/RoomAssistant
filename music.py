6d985179d33809d8c8dec9f8394afd4192329c13
sh4d0w <sh4d0w@sh4d0w.sh4d0w>
2021-12-17 18:45:02 +0100

    Added close function to music module

--- music.py
+++ music.py
@@ -1,12 +1,6 @@
-import json
 import spotipy
 import webbrowser
-
-username = '31cvpctvdtoh57kzurnv3emafqja'
-clientId = '9dad35d7cc144001b472518b9ac6d738'
-clientSecret = '95bf4812e4af41d695850121273c9344'
-redirectURI = 'https://google.com/'
-
+import os
 
 class useSpotify:
     def __init__(self):
@@ -17,7 +11,7 @@ class useSpotify:
 
     def getToken(self):
         # Create OAuth Object
-        oauth_object = spotipy.SpotifyOAuth(clientId,clientSecret,redirectURI)
+        oauth_object = spotipy.SpotifyOAuth(self.clientId, self.clientSecret, self.redirectURI)
         #Create token
         token_dict = oauth_object.get_access_token()
         token = token_dict['access_token']
@@ -29,42 +23,10 @@ class useSpotify:
     def use(self):
         self.getToken()
         print("Welcome, " + self.user['display_name'])
-        print("0 - Exit")
-        print("1 - Search for a song")
-        choice = int(input("Your choice: "))
-        if(choice == 1):
-            #Get song name
-            searchQuery = input("Enter Song Name: ")
-            #Search for the song
-            searchResults = self.spotifyObject.search(searchQuery,1,0,"track")
-            #Get required data from JSON respone
-            tracks_dict = searchResults['tracks']
-            tracks_items = tracks_dict['items']
-            song = tracks_items[0]['external_urls']['spotify']
-            #Open the song in webbrowser
-            webbrowser.open(song)
-            print('Song has opened in your browser')
-        elif(choice == 0):
-            return 0
-        else:
-            print("Enter valid choice")
-
-x = useSpotify()
-x.use()
-
-#To print the response in readable format
-#print(json.dumps(user,sort_keys=True, indent=4))
-
-'''while True:
-    print("Welcome, " + user['display_name'])
-    print("0 - Exit")
-    print("1 - Search for a song")
-    choice = int(input("Your choice: "))
-    if(choice == 1):
         #Get song name
         searchQuery = input("Enter Song Name: ")
         #Search for the song
-        searchResults = spotifyObject.search(searchQuery,1,0,"track")
+        searchResults = self.spotifyObject.search(searchQuery,1,0,"track")
         #Get required data from JSON respone
         tracks_dict = searchResults['tracks']
         tracks_items = tracks_dict['items']
@@ -72,7 +34,6 @@ x.use()
         #Open the song in webbrowser
         webbrowser.open(song)
         print('Song has opened in your browser')
-    elif(choice == 0):
-        break
-    else:
-        print("Enter valid choice")'''
+
+    def closeMusic(self, name):
+        os.system("killall -9 " + name)
