77f7fa44d0ffe8852c2dabdca5985873c8e2ef79
root <root@sh4d0w.sh4d0w>
2022-01-18 23:50:12 +0100

    Added programming jokes and Wikipedia

--- fun.py
+++ fun.py
@@ -1,5 +1,9 @@
 import gtts
 from playsound import playsound
+import pyjokes
+from googletrans import Translator
+import wikipediaapi
+import talk
 
 
 def redAlert():
@@ -7,3 +11,22 @@ def redAlert():
     tts.save("Audio/redalert.wav")
     playsound("Audio/redalert.wav")
     playsound("Audio/doom.wav")
+
+def tellJoke():
+    translator = Translator()
+    joke = pyjokes.get_joke()
+    result = translator.translate(joke, dest='pl')
+    print(joke)
+    print(result.text)
+    talk.tellSentence(result.text)
+
+
+def getWikipediaInfo(subject):
+    wiki = wikipediaapi.Wikipedia('pl')
+    page_py = wiki.page(subject)
+    if(page_py.summary == ""):
+        talk.tellSentence("Nie znalazłem żadnych informacji")
+    else:
+        talk.tellSentence(page_py.summary)
+
+tellJoke()
