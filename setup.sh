28065fcf4eb2f6d871f707136074e35f6f7b53df
root <root@sh4d0w.sh4d0w>
2022-01-21 14:10:02 +0100

    Run to install neccessary modules

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install SpeechRecognition==3.8.1
+pip install pymysql
+pip install gtts==2.2.3
+pip install playsound==1.3.0
+pip install pyjokes
+pip install googletrans==3.1.0-alpha
+pip install Wikipedia-API
