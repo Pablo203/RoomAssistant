c372af6a19337d574370e18b3c0c52769a274927
sh4d0w <sh4d0w@pop-os.localdomain>
2022-02-24 19:48:41 +0100

    Update setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 pip install SpeechRecognition==3.8.1
+pip install gobject #TEMP
 pip install pymysql
 pip install gtts==2.2.3
 pip install playsound==1.3.0
 pip install pyjokes
 pip install googletrans==3.1.0-alpha
 pip install Wikipedia-API
+sudo apt install portaudio19-dev -y
+pip install pyaudio
